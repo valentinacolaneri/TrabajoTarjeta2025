b50e9227d24bd7ad6927dbe23785d6a52a5fce79
informatica <colanerivalen2006@gmail.com>
2025-09-16 16:04:30 -0300

    Create Colectivo.cs
    clase colectivo1

---
+++ TarjetaSube/Colectivo.cs
@@ -0,0 +1,25 @@
+﻿public class Colectivo
+{
+    private const decimal TARIFA_BASICA = 1580m;
+    private string linea;
+
+    public Colectivo(string linea)
+    {
+        this.linea = linea;
+    }
+
+    public Boleto PagarCon(Tarjeta tarjeta)
+    {
+        decimal montoPasaje = tarjeta.CalcularMontoPasaje(TARIFA_BASICA);
+
+        if (tarjeta.PuedePagar(TARIFA_BASICA))
+        {
+            tarjeta.Descontar(montoPasaje);
+            return new Boleto(montoPasaje, linea, DateTime.Now, true);
+        }
+        else
+        {
+            return new Boleto(montoPasaje, linea, DateTime.Now, false);
+        }
+    }
+}
