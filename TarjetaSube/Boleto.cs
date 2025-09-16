1cbd6acf3fdc2968c8c7d9c205afc51ffccc60d8
informatica <colanerivalen2006@gmail.com>
2025-09-16 15:55:24 -0300

    Update Boleto.cs
    clase boleto1

--- TarjetaSube/Boleto.cs
+++ TarjetaSube/Boleto.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TarjetaSube
+public class Boleto
 {
-    class Class1
+    public decimal Monto { get; }
+    public string LineaColectivo { get; }
+    public DateTime FechaHora { get; }
+    public bool EsValido { get; }
+
+    public Boleto(decimal monto, string lineaColectivo, DateTime fechaHora, bool esValido)
     {
+        Monto = monto;
+        LineaColectivo = lineaColectivo;
+        FechaHora = fechaHora;
+        EsValido = esValido;
+    }
+
+    public override string ToString()
+    {
+        return $"Boleto - Línea: {LineaColectivo}, Monto: ${Monto}, " +
+               $"Fecha: {FechaHora:dd/MM/yyyy HH:mm}, Válido: {EsValido}";
     }
 }
